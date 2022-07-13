ccc9a4adc1affe501e0c831be92d231beae78be2
xenakios <xenakios@gmail.com>
2022-07-13 06:22:23 +0300

    initial git commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+KeyboardComponentExampleAudioProcessorEditor::KeyboardComponentExampleAudioProcessorEditor (KeyboardComponentExampleAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p),
+    mKeyComponent(p.mKeyState,juce::MidiKeyboardComponent::horizontalKeyboard)
+{
+    addAndMakeVisible(mKeyComponent);
+    setSize (400, 300);
+}
+
+KeyboardComponentExampleAudioProcessorEditor::~KeyboardComponentExampleAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void KeyboardComponentExampleAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void KeyboardComponentExampleAudioProcessorEditor::resized()
+{
+    mKeyComponent.setBounds(0, getHeight()-50, getWidth(), 50);
+}
